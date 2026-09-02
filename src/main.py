825b5b6a34062bd28a7eb523d58cddbc1bbe397a
morteza rostami <morteza.rostami.cs@gmail.com>
2026-09-02 22:53:12 +0400

    basic frontend component and state pattern

--- src/main.py
+++ src/main.py
@@ -5,12 +5,20 @@ from fastapi.responses import FileResponse
 from src.users.routes import register_user_routes
 # error handler
 from src.common.handlers import register_exception_handlers
+from fastapi.staticfiles import StaticFiles
 
 # get parent dir of main.py
 BASE_DIR = Path(__file__).resolve().parent
 
 # fastapi main app
 app = FastAPI()
+
+# serve static files
+app.mount(
+   "/static",
+   StaticFiles(directory=BASE_DIR / "frontend"),
+   name="static",
+)
 
 # register global error handler
 register_exception_handlers(app=app)
@@ -23,3 +31,7 @@ register_user_routes(app=app)
 def home():
    # serve html file
    return FileResponse(BASE_DIR / "frontend" / "index.html")
+
+@app.get("/register", include_in_schema=False)
+def register_page():
+   return FileResponse(BASE_DIR / "frontend" / "register.html")
